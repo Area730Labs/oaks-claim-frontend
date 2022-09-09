903aabf084b550944bfba5bccf09c33f26c96c30
Vladyslav Melnychenko <webmonch@gmail.com>
2022-09-09 22:26:49 +0200

    upd

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreBuildErrors: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
